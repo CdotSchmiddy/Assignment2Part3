7cbfbb04e6a026b168d97687621c1c8b7a207e7e
Calum Schmidtke <200366773@student.georgianc.on.ca>
2018-12-06 13:32:34 -0500

    Add project files.

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+//reference models
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private MovieListingsModel db;
+
+        //constructor - connect to db as soon as this controller is instantiated
+        public MoviesController(MovieListingsModel db)
+        {
+            this.db = db;
+        }
+
+        // GET: /api/movies
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return db.Movies.OrderBy(a => a.Title).ToList();
+        }
+
+        // GET: /api/movies/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            Movie movie = db.Movies.SingleOrDefault(a => a.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(movie);
+            }
+        }
+
+        // POST: /api/movies/5
+        [HttpGet("{id}")]
+        public ActionResult Post([FromBody] Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return CreatedAtAction("Post", movie);
+            }
+        }
+
+        // PUT: api/movies/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                db.Entry(movie).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                db.SaveChanges();
+                return AcceptedAtAction("Put", movie);
+            }
+        }
+
+        // DELETE: api/movies/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            Movie movie = db.Movies.SingleOrDefault(a => a.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                db.Movies.Remove(movie);
+                db.SaveChanges();
+                return Ok();
+            }
+        }
+
+    }
+}
---
+++ WebApplication1/Models/MovieListingsModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class MovieListingsModel : DbContext
+    {
+        //constructor that reads the db connection string
+        public MovieListingsModel(DbContextOptions<MovieListingsModel>options): base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
